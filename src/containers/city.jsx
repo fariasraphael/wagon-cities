beeb4d951ab739e518b0f4ba4ff99e6da1953a79
Raphael Farias <farias.raphael@gmail.com>
2024-02-25 23:26:00 -0300

    update city component

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { activeCity } from '../actions';
+
 
 class City extends Component {
-	render() {
-		return(
-			<div className="list-group-item">
-				{this.props.city.name}
-			</div>
-		)
+	handleClick = () => {
+		// Redux Action
+		
+		this.props.activeCity(this.props.city)
+		console.log(this.props.activeCity);
 	}
+
+
+	render() {
+        let classes = "list-group-item";
+        if (this.props.city === this.props.activeCity) {
+            classes += " selected";
+        }
+
+        return (
+            <div className={classes} onClick={this.handleClick}>
+                {this.props.city.name}
+            </div>
+        );
+    }
 }
 
-export default City;
+
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(
+        { activeCity: activeCity },
+        dispatch
+    );
+}
+
+function mapReduxStateToProps(reduxState) {
+    return {
+        activeCity: reduxState.activeCity
+    };
+}
+
+export default connect(mapReduxStateToProps, mapDispatchToProps)(City);
